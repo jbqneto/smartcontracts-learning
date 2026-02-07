db0f5080e71aab02d13996becba5b08804a7e4d7
jbqneto <jbqneto@gmail.com>
2026-02-07 16:45:18 +0000

    Contract updated

--- contracts/Webbb3.sol
+++ contracts/Webbb3.sol
@@ -1,3 +1,36 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.0.21;
+pragma solidity ^0.8.21;
+
+struct Voting {
+    string option1;
+    uint votes1;
+    string option2;
+    uint votes2;
+    uint maxDate;
+}
+
+contract Webbb3 {
+    address owner;
+    uint public currentVoting;
+    Voting[] public votings;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getCurrentVoting() public view returns (Voting memory) {
+        return votings[currentVoting];
+    }
+
+    function addVoting(string memory option1, string memory option2, uint timeToVote) public {
+        require(msg.sender == owner, "Only owner can add a new voting");
+        
+        Voting memory vote;
+        vote.option1 = option1;
+        vote.option2 = option2;
+        vote.maxDate = timeToVote + block.timestamp;
+        votings.push(vote);
+        currentVoting = votings.length - 1;
+    }
+}
